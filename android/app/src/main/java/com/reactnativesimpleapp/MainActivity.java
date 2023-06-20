e012893b02299a57f6630456d741a63a4cfc7bcc
Dgreasidis <dgreasidis+github@gmail.com>
2023-06-20 11:59:45 +0300

    chore: remove unneeded imports

--- android/app/src/main/java/com/reactnativesimpleapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativesimpleapp/MainActivity.java
@@ -3,7 +3,6 @@ import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
     /**
